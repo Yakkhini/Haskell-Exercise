590bc83974eda58783512cdce4cad58b2a07a6e0
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-08-28 21:40:58 +0800

    flake.nix: use short pkgs & stack tool.

--- flake.nix
+++ flake.nix
@@ -6,14 +6,22 @@
   outputs = {
     self,
     nixpkgs,
-  }: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    formatter.x86_64-linux = pkgs.alejandra;
 
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.haskellPackages.shellFor {
+    devShells.x86_64-linux.default = pkgs.haskellPackages.shellFor {
       name = "haskell-dev";
+
       packages = hpkgs: [
         # reuse the nixpkgs for this package
         # hpkgs.distribution-nixpkgs
+      ];
+
+      buildInputs = [
+        pkgs.exercism
+        pkgs.stack
       ];
     };
   };
